5db51823805d0485f9e6498d44b99f005c96b563
painnick <painnick@gmail.com>
2022-12-14 01:05:38 +0900

    BUGFIX. ledc setup

--- tank-idf/main/TrackController.cpp
+++ tank-idf/main/TrackController.cpp
@@ -9,8 +9,8 @@
 #define TRACK_MOTOR_RESOLUTION 8
 
 TrackController::TrackController(int pin1, int pin2, int ledc1, int ledc2) : ledc1(ledc1), ledc2(ledc2), speed(255) {
-    ledcSetup(pin1, 1000, TRACK_MOTOR_RESOLUTION);
-    ledcSetup(pin2, 1000, TRACK_MOTOR_RESOLUTION);
+    ledcSetup(ledc1, 1000, TRACK_MOTOR_RESOLUTION);
+    ledcSetup(ledc2, 1000, TRACK_MOTOR_RESOLUTION);
 
     ledcAttachPin(pin1, ledc1);
     ledcAttachPin(pin2, ledc2);
